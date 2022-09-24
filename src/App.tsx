d410e983287a22888669681131e1bef8b2e2d2cb
Medas <medas02345@gmail.com>
2022-09-24 15:07:10 +0300

    Add gradient background colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import Header from './Header';
+import React from "react";
+import Header from "./Header";
 
 const App: React.FC = () => {
   return (
-    <div className='bg-stone-100 dark:bg-gradient-to-tr from-gray-700 via-gray-900 to-black h-screen'>
-     <Header></Header>
+    <div className="bg-gradient-to-b from-gray-100 to-gray-300 dark:bg-gradient-to-tr dark:from-gray-700 dark:via-gray-900 dark:to-black h-screen">
+      <Header></Header>
     </div>
   );
-}
+};
 
 export default App;
--- src/Header.tsx
+++ src/Header.tsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import ToggleButton from './ToggleButton'
+import React from "react";
+import ToggleButton from "./ToggleButton";
 
 const Header: React.FC = () => {
   return (
-    <div className='h-24 bg-stone-50 dark:bg-gradient-to-r from-gray-700 via-gray-900 to-black flex justify-center items-center border-b-2 border-indigo-400 shadow-xl relative'>
-        <h2 className='text-indigo-500 text-3xl font-extrabold drop-shadow-lg font-righteous'>Infinite Gallery</h2>
-        <ToggleButton></ToggleButton>
+    <div className="h-24 bg-gradient-to-b from-gray-100 to-gray-300 dark:from-gray-700 dark:via-gray-900 dark:to-black flex justify-center items-center border-b-2 border-indigo-400 shadow-xl relative">
+      <h2 className="text-indigo-500 text-3xl font-extrabold drop-shadow-lg font-righteous">
+        Infinite Gallery
+      </h2>
+      <ToggleButton></ToggleButton>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
